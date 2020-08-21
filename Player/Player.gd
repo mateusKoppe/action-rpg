7339daed84c59b8c92f6104acce6ce97728e559c
Mateus Koppe <mateus.koppe@gmail.com>
2020-08-20 23:32:22 -0300

    Setup tilemaps

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,12 +4,33 @@ const ACCELERATION = 10
 const FRICTION = 10
 const MAX_SPEED = 80
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
 var velocity = Vector2.ZERO
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
+func _ready():
+	animationTree.active = true
+
 func _physics_process(_delta):
+	match state:
+		MOVE:
+			state_move()
+		
+		ATTACK:
+			pass
+			
+		ROLL:
+			pass
+
+func state_move():
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
